b500ee264871e0ad6d342c642a5a3b5d67e30158
yihu-lv <1720427782@qq.com>
2021-04-13 10:52:05 +0800

    打包css ，js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 var path = require('path');
 
 module.exports = {
@@ -5,6 +6,18 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins:[new HtmlWebpackPlugin({
+    title: 'webpack使用说明',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
